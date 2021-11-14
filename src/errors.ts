038af7eef5c805ae675f70abf1a6e025ca7f4e28
Zedekul <zedekul@pm.me>
2021-11-14 05:02:40 +0000

    Add skip logging attribute to errors.

--- src/errors.ts
+++ src/errors.ts
@@ -5,6 +5,7 @@ export class DedeletedError extends Error {
 
   public code = 0
   public isDedeletedError = true
+  public skipLogging = true
   public get isPrivate(): boolean { return this.code < 0 }
 }
 
